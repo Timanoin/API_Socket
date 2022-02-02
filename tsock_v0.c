900a46eabf47d41a96683b9c8468c7eb875ba910
Timanoin <arthur_gautheron@yahoo.fr>
2022-02-02 10:19:03 +0100

    Structure UDP/TCP

--- tsock_v0.c
+++ tsock_v0.c
@@ -18,6 +18,12 @@ données du réseau */
 /* pour la gestion des erreurs */
 #include <errno.h>
 
+// Construit un message en répétant lg-5 fois un motif
+void construire_message (char *message, char motif, int lg);
+
+void afficher_message (char *message, int lg);
+
+
 void main (int argc, char **argv)
 {
 	int c;
@@ -25,56 +31,111 @@ void main (int argc, char **argv)
 	extern int optind;
 	int nb_message = -1; /* Nb de messages à envoyer ou à recevoir, par défaut : 10 en émission, infini en réception */
 	int source = -1 ; /* 0=puits, 1=source */
-	while ((c = getopt(argc, argv, "pn:s")) != -1) {
-		switch (c) {
-		case 'p':
-			if (source == 1) {
+	int udp = 0 // 0=tcp, 1=udp
+
+	while ((c = getopt(argc, argv, "pn:s")) != -1) 
+	{
+		switch (c) 
+		{
+			case 'p':
+				if (source == 1) 
+				{
+					printf("usage: cmd [-p|-s][-n ##]\n");
+					exit(1);
+				}
+				source = 0;
+				break;
+
+			case 's':
+				if (source == 0) 
+				{
+					printf("usage: cmd [-p|-s][-n ##]\n");
+					exit(1) ;
+				}
+				source = 1;
+				break;
+
+			case 'n':
+				nb_message = atoi(optarg);
+				break;
+
+			case 'u':
+				udp = 1;
+				break;
+
+			default:
 				printf("usage: cmd [-p|-s][-n ##]\n");
-				exit(1);
-			}
-			source = 0;
-			break;
-
-		case 's':
-			if (source == 0) {
-				printf("usage: cmd [-p|-s][-n ##]\n");
-				exit(1) ;
-			}
-			source = 1;
-			break;
-
-		case 'n':
-			nb_message = atoi(optarg);
-			break;
-
-		default:
-			printf("usage: cmd [-p|-s][-n ##]\n");
-			break;
+				break;
 		}
 	}
 
-	if (source == -1) {
+	// Oubli de préciser puits ou source
+	if (source == -1) 
+	{
 		printf("usage: cmd [-p|-s][-n ##]\n");
 		exit(1) ;
 	}
 
 	if (source == 1)
+	 {
 		printf("on est dans le source\n");
-	else
-		printf("on est dans le puits\n");
+		// UDP
+		if (udp)
+		{
+			sock = socket(AF_INET, SOCK_DGRAM, UDP)
+		}
+		// TCP
+		else
+		{
 
-	if (nb_message != -1) {
+		}
+	 }
+	else
+	{
+		printf("on est dans le puits\n");
+		// UDP
+		if (udp)
+		{
+
+		}
+		// TCP
+		else
+		{
+
+		}
+	}
+
+	if (nb_message != -1) 
+	{
 		if (source == 1)
 			printf("nb de tampons à envoyer : %d\n", nb_message);
 		else
 			printf("nb de tampons à recevoir : %d\n", nb_message);
-	} else {
-		if (source == 1) {
+	} 
+	else 
+	{
+		if (source == 1) 
+		{
 			nb_message = 10 ;
 			printf("nb de tampons à envoyer = 10 par défaut\n");
-		} else
-		printf("nb de tampons à envoyer = infini\n");
-
+		} 
+		else printf("nb de tampons à envoyer = infini\n");
 	}
 }
 
+// Construit un message en répétant lg-5 fois un motif
+void construire_message (char *message, char motif, int lg) 
+{
+	int i;
+	for (i=0 ; i<lg; i++) message[i] = motif; 
+}
+
+void afficher_message (char *message, int lg) {
+	int i;
+	printf("message construit : ");
+	for (i=0 ; i<lg ; i++) 
+	{
+		printf("%c", message[i]) ; 
+		printf("\n");
+	}
+}
